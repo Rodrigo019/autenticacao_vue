80750f67b82f95e4ecfa155a64465502602b4007
Rodrigo Otávio Belo <rodrigootavio019@gmail.com>
2021-09-23 11:49:13 -0300

    continue

---
+++ src/modules/user/pages/UserCreate.vue
@@ -0,0 +1,25 @@
+<template>
+    teste
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UserService from '../services/userservice'
+import User from '@/modules/user/models/user'
+
+export default defineComponent({
+    name: 'UserCreate',
+    data() {
+        return {
+            user: User
+        }
+    },
+    async created() {
+        const id = this.$route.params['id'].toString();
+        console.log(id);
+        const response = await UserService.get(Number.parseInt(id));
+        console.log(response);
+        this.user = response.retorno
+    }
+})
+</script>
